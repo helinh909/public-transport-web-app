7e42d0d1271d71ea675833129412a54ca7704c1b
Emeline <emeline.baudin@etudiant.univ-lr.fr>
2024-01-03 20:17:17 +0100

    finalisation

--- js/carte.js
+++ js/carte.js
@@ -1,4 +1,4 @@
-console.log("Exécution du programme carte.js");
+//console.log("Exécution du programme carte.js");
 
 //Creation de la carte
 maCarte = L.map("map").setView([46.148358, -1.156659], 12.5);
@@ -699,26 +699,3 @@ traiterDonnees();
 
 //------------------------------------------------------
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal);
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  // console.log(e.key);
-
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
